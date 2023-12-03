964e1e86003e0065a2b9e816ecca5f7446b75988
Mathias.Mouillard <Mouillardpro@gmail.com>
2023-12-03 16:47:54 +0100

    Replace html to api gallery

--- FrontEnd/Js/api.js
+++ FrontEnd/Js/api.js
@@ -1,37 +1,43 @@
 const apiUrl = 'http://localhost:5678/api';
 
-fetch(apiUrl)
-  .then(response => {
+  async function getUserLoginFromApi() {
+    try {
+      const response = await fetch(apiUrl + "/user/login");
+      if (!response.ok) {
+        throw new Error('Erreur de récupération des données');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erreur de récupération des données :', error.message);
+      return [];
+    }
+  };
+
+  async function getCategoriesFromApi() {
+    try {
+    const response = await fetch(apiUrl + "/Categories");
     if (!response.ok) {
       throw new Error('Erreur de récupération des données');
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Erreur de récupération des données :', error);
-  });
-
-
-  async function getUserLogin() {
-    const response = await fetch(apiUrl + "/user/login")
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erreur de récupération des données :', error.message);
+    return [];
   }
-
-  async function getCategories() {
-    const response = await fetch(apiUrl + "/Categories")
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
-  }
-
-  async function getWorks() {
-    const response = await fetch(apiUrl + "/works")
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
-  }
+};
+   
+  async function getWorksFromApi() {
+    try {
+      const response = await fetch(apiUrl + "/works");
+      if (!response.ok) {
+        throw new Error('Erreur de récupération des données');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erreur de récupération des données :', error.message);
+      return [];
+    }
+  };
--- FrontEnd/Js/main.js
+++ FrontEnd/Js/main.js
@@ -1,8 +1,20 @@
-//création fichier travaux
-
-
-async function init() {
-   await getWorks()
-}
-
-init();
+// Gallery - show gallery
+async function initWorks() {
+   const data = await getWorksFromApi();
+ 
+   const galleryElement = document.querySelector('.gallery');
+   let galleryHTML = '';
+ 
+   data.forEach(item => {
+     galleryHTML += `
+       <figure>
+         <img src="${item.imageUrl}">
+         <figcaption>${item.title}</figcaption>
+       </figure>
+     `;
+   });
+ 
+   galleryElement.innerHTML = galleryHTML;
+ }
+ 
+ initWorks();
