ea95f0f4ff1c09833b2838995d7eb72f0cb0c15b
Mathias.Mouillard <Mouillardpro@gmail.com>
2023-12-08 13:59:06 +0100

    Main js clear order

--- FrontEnd/Js/main.js
+++ FrontEnd/Js/main.js
@@ -1,3 +1,42 @@
+// Conteneur des categories
+const buttonsContainer = document.createElement("div");
+buttonsContainer.className = "buttons-container";
+const categories = ["Tous", "Objets", "Appartements", "Hôtels & restaurants"];
+
+let activeButton = null;
+
+// Place la balise des boutons dans le html
+const portfolioSection = document.querySelector("#portfolio");
+if (portfolioSection) {
+   const h2Element = portfolioSection.querySelector("h2");
+   if (h2Element) {
+     h2Element.insertAdjacentElement("afterend", buttonsContainer);
+   }
+ }
+
+// Crée les boutons par rapport au tableau des categories
+categories.forEach((category, index) => {
+   let button = document.createElement("button");
+   button.textContent = category;
+   button.className = "filter__btn";
+   button.setAttribute('data-category-id', index);
+
+   buttonsContainer.appendChild(button);
+
+   button.addEventListener("click", function() {
+     const categoryId = parseInt(this.getAttribute('data-category-id'));
+
+     if (activeButton) {
+       activeButton.classList.remove("filter__btn--active");
+     }
+
+     this.classList.add("filter__btn--active");
+     activeButton = this;
+
+     filterWorksByCategory(categoryId);
+   });
+ });
+
 // Filtre par categorie
 async function filterWorksByCategory(categoryId) {
    const data = await getWorksFromApi();
@@ -24,6 +63,11 @@ async function filterWorksByCategory(categoryId) {
    galleryElement.innerHTML = galleryHTML;
  }
 
+ // Active premier bouton par défaut
+const firstButton = buttonsContainer.querySelector("button[data-category-id='0']");
+firstButton.classList.add("filter__btn--active");
+activeButton = firstButton;
+
 // Affiche la galerie
 function displayAllWorks(works) {
    const galleryElement = document.querySelector('.gallery');
@@ -41,48 +85,7 @@ function displayAllWorks(works) {
    galleryElement.innerHTML = galleryHTML;
  }
 
-// Conteneur des categories
-var buttonsContainer = document.createElement("div");
-buttonsContainer.className = "buttons-container";
-var categories = ["Tous", "Objets", "Appartements", "Hôtels & restaurants"];
-
-var activeButton = null;
-
-// Crée les boutons par rapport au tableau des categories
-categories.forEach((category, index) => {
-   var button = document.createElement("button");
-   button.textContent = category;
-   button.className = "filter__btn";
-   button.setAttribute('data-category-id', index);
-
-   buttonsContainer.appendChild(button);
-
-   button.addEventListener("click", function() {
-     const categoryId = parseInt(this.getAttribute('data-category-id'));
-
-     if (activeButton) {
-       activeButton.classList.remove("filter__btn--active");
-     }
-
-     this.classList.add("filter__btn--active");
-     activeButton = this;
-
-     filterWorksByCategory(categoryId);
-   });
- });
-
-// Active premier bouton par défaut
-var firstButton = buttonsContainer.querySelector("button[data-category-id='0']");
-firstButton.classList.add("filter__btn--active");
-activeButton = firstButton;
-
-// Place la balise des boutons dans le html
-var portfolioSection = document.querySelector("#portfolio");
-if (portfolioSection) {
-   var h2Element = portfolioSection.querySelector("h2");
-   if (h2Element) {
-     h2Element.insertAdjacentElement("afterend", buttonsContainer);
-   }
- }
-
+// Active la galerie
 filterWorksByCategory(0);
+
+
