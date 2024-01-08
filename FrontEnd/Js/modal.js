8029c563dc2b9094c4fed306ff717383224a6fb4
Mathias.Mouillard <Mouillardpro@gmail.com>
2024-01-08 08:50:39 +0100

    Modal working progress

--- FrontEnd/Js/modal.js
+++ FrontEnd/Js/modal.js
@@ -2,8 +2,12 @@ import { getWorks } from './api.js';
 
 const btnEditModal = document.querySelector(".btnEditModal");
 const aside1 = document.getElementById("aside1");
-const modalCloseBtn = document.querySelector("#aside1 .modal-close");
+const aside2 = document.getElementById("aside2");
+const modal1CloseBtn = document.querySelector("#aside1 .modal-close");
+const modal2CloseBtn = document.querySelector("#aside2 .modal-close");
 const modalGallery = document.querySelector("#aside1 .modalGallery");
+
+// Modal 1 Gallery 
 
 // Ouverture Modal de Gallerie
 btnEditModal.addEventListener("click", async function() {
@@ -11,12 +15,7 @@ btnEditModal.addEventListener("click", async function() {
   document.body.classList.add("modal-open");
   await fetchAndDisplayGallery();
 });
-// fermeture Modal de Gallerie
-modalCloseBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  aside1.style.display = "none";
-  document.body.classList.remove("modal-open");
-});
+
 
 // Fonction pour charger et afficher la galerie
 async function fetchAndDisplayGallery() {
@@ -41,3 +40,44 @@ async function fetchAndDisplayGallery() {
   }
 }
 
+// Fonction pour afficher ou masquer un modal
+function toggleModal(modal) {
+    if (modal.style.display === "none" || modal.style.display === "") {
+      modal.style.display = "block";
+    } else {
+      modal.style.display = "none";
+    }
+  }
+  
+// Redirection to Modal 2
+const addPhotoBtn = document.getElementById("addPhotoBtn");
+
+addPhotoBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  aside1.style.display = "none";
+  aside2.style.display = "block";
+});
+  
+// Redirection to Modal 1
+const modalArrow = document.querySelector(".modal-arrow");
+modalArrow.addEventListener("click", function(event) {
+  event.preventDefault();
+  aside1.style.display = "block";
+  aside2.style.display = "none";
+});
+  
+
+
+// Modal Gobal
+
+// fermeture Modal
+modal1CloseBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    aside1.style.display = "none";
+    document.body.classList.remove("modal-open");
+});
+modal2CloseBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    aside2.style.display = "none";
+    document.body.classList.remove("modal-open");
+});
