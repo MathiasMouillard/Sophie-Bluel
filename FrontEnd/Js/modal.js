27f785939c560b934d4522d4698bf93756bc5029
Mathias.Mouillard <Mouillardpro@gmail.com>
2024-01-08 06:47:02 +0100

    add gallery and trash in modal1

--- FrontEnd/Js/modal.js
+++ FrontEnd/Js/modal.js
@@ -1,7 +1,43 @@
-const btnEditModal = document.querySelector(".btnEditModal")
+import { getWorks } from './api.js';
 
-btnEditModal.addEventListener("click", function() {
-    // Actions à effectuer lorsque le bouton est cl
-    alert("Le bouton btnEditMod a été cliqué !");
-  });
-  
+const btnEditModal = document.querySelector(".btnEditModal");
+const aside1 = document.getElementById("aside1");
+const modalCloseBtn = document.querySelector("#aside1 .modal-close");
+const modalGallery = document.querySelector("#aside1 .modalGallery");
+
+// Ouverture Modal de Gallerie
+btnEditModal.addEventListener("click", async function() {
+  aside1.removeAttribute("style");
+  document.body.classList.add("modal-open");
+  await fetchAndDisplayGallery();
+});
+// fermeture Modal de Gallerie
+modalCloseBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  aside1.style.display = "none";
+  document.body.classList.remove("modal-open");
+});
+
+// Fonction pour charger et afficher la galerie
+async function fetchAndDisplayGallery() {
+  try {
+    const galleryData = await getWorks();
+
+    let galleryHTML = '';
+    galleryData.forEach(item => {
+      galleryHTML += `
+      <div class="image-container">
+      <img src="${item.imageUrl}">
+      <div class="delete-background">
+        <i class="fa-solid fa-trash-can delete-icon"></i>
+      </div>
+    </div>
+      `;
+    });
+
+    modalGallery.innerHTML = galleryHTML;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données de la galerie :", error);
+  }
+}
+
